337832a8a35a52689e8adc079087731d3e6ffea1
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-04 22:08:55 -0600

    test cipher has shifted values.

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -15,4 +15,8 @@ class CipherTest < Minitest::Test
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, @cipher.characters
   end
+
+  def test_cipher_has_shited_values
+    assert_equal [3, 27, 73, 20], @cipher.shift_values("02715", "040895")
+  end
 end
