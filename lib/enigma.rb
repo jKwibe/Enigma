ca5db2a7cb7b95f38b6a638879e3881cd0467883
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-05 07:17:21 -0600

    Run the cipher#encrypt to run the file.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,8 @@
-
+require_relative  "./cipher"
 class Enigma
+  def initialize
+    @cipher= Cipher.new
+  end
   def date_gen
     Date.today.strftime("%d%m%y")
   end
@@ -7,5 +10,9 @@ class Enigma
   def gen_keys
     random_num_arr =  5.times.map{ rand(10) }
     random_num_arr.join
+  end
+
+  def encrypt(message, key = gen_keys, date = date_gen)
+    @cipher.encrypt(message, key, date)
   end
 end
