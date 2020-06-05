4fc6d6301c0ea349d9e66e99086cbc08ec04fcf2
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-04 22:19:33 -0600

    Mthods to display the encrypted and decrypted messages.

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -4,4 +4,20 @@ class Cipher < Shift
   def characters
     ("a".."z").to_a << " "
   end
+
+  def encrypted_message(message, key, date)
+    {
+      encryption: message ,
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypted_message(message, key, date)
+    {
+      decryption: message ,
+      key: key,
+      date: date
+    }
+  end
 end
