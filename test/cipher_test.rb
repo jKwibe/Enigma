5434fffaf95b96061d64af3daea6586a81d863f9
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-04 22:06:18 -0600

    test if the cipher has all the characters.

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -10,4 +10,9 @@ class CipherTest < Minitest::Test
   def test_it_exists
     assert_instance_of Cipher, @cipher
   end
+
+  def test_cipher_has_all_characters
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @cipher.characters
+  end
 end
