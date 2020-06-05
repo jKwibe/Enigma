babdc85274b7095c43661912582ed88ea742bd21
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-05 11:49:57 -0600

    Refactored the enigma class to uinherit from Cipher class.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,23 +1,4 @@
 require_relative  "./cipher"
-class Enigma
-  def initialize
-    @cipher= Cipher.new
-  end
-  
-  def date_gen
-    Date.today.strftime("%d%m%y")
-  end
+class Enigma < Cipher
 
-  def gen_keys
-    random_num_arr =  5.times.map{ rand(10) }
-    random_num_arr.join
-  end
-
-  def encrypt(message, key = gen_keys, date = date_gen)
-    @cipher.encrypt(message, key, date)
-  end
-
-  def decrypt(message, key = gen_keys, date = date_gen)
-    @cipher.decrypt(message, key, date)
-  end
 end
