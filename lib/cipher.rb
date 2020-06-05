21d7caafcca67ac9ab10e97f7a79c8e84013a0a6
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-05 11:52:01 -0600

    Include the module in the cipher class.

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,8 +1,10 @@
 require_relative "./shift"
 require_relative "./modules/displayable"
+require_relative "./modules/generatable"
 
 class Cipher < Shift
   include Displayable
+  include Generatable
   def characters
     ("a".."z").to_a << " "
   end
@@ -17,20 +19,20 @@ class Cipher < Shift
     Hash[group_matches]
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = gen_keys, date = date_gen)
     encrypted_text = ""
     count = 0
-    message.chars do |char|
+    message.downcase.chars do |char|
       encrypted_text << encrypt_shift_matches(key, date, count)[char]
       count.eql?(3) ? count = 0 : count+= 1
     end
     encrypted_message(encrypted_text, key, date)
   end
 
-  def decrypt(message, key, date)
+  def decrypt(message, key = gen_keys, date = date_gen)
     encrypted_text = ""
     count = 0
-    message.chars do |char|
+    message.downcase.chars do |char|
       encrypted_text << decrypt_shift_matches(key, date, count)[char]
       count.eql?(3) ? count = 0 : count+= 1
     end
