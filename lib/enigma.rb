59fbe24b13bbe165bbaa606a3201c993908b2f93
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-05 00:02:18 -0600

    Change date name to prevent naming conflicts. Generate five random numbers.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,11 @@
 
 class Enigma
-
-  def date
+  def date_gen
     Date.today.strftime("%d%m%y")
   end
 
+  def gen_keys
+    random_num_arr =  5.times.map{ rand(10) }
+    random_num_arr.join
+  end
 end
