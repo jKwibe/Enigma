ccb119f7b97c9d8ed37b7ed3f9990c1f424fed4a
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-04 20:57:17 -0600

    Test if the class exists

---
+++ test/shift_test.rb
@@ -0,0 +1,14 @@
+
+
+require_relative "./helper"
+require "./lib/shift"
+
+class ShiftTest < Minitest::Test
+  def setup
+    @shift = Shift.new
+  end
+
+  def test_it_exists
+    assert_instance_of Shift, @shift
+  end
+end
