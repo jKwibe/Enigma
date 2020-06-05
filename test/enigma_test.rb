43d0df9293dabb945e2100fc2683e929c22bf415
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-05 07:15:47 -0600

    Add a test to check whether the message has been encrypted.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,12 +12,24 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_generate_time_string
-    Date.today.stubs(:strftime).returns("040620")
-    assert_equal "040620", @enigma.date_gen
+    Date.stubs(:today).returns(Date.new(2020, 06, 05))
+    assert_equal "050620", @enigma.date_gen
   end
 
   def test_generate_5_random_numbers
     @enigma.stubs(:gen_keys).returns("23456")
     assert_equal "23456", @enigma.gen_keys
+  end
+
+  def test_machine_can_encrypt
+    Date.stubs(:today).returns(Date.new(2020, 06, 05))
+    @enigma.stubs(:gen_keys).returns("23456")
+    expected = {
+                  :encryption=>"hpcnoknqrwv",
+                  :key=>"23456",
+                  :date=>"050620"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world")
   end
 end
