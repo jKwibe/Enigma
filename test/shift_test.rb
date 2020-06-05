f8f91a8671afa017da96360b1715330f6de58ed2
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-04 21:09:32 -0600

    test shift can get the keys.

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -11,4 +11,10 @@ class ShiftTest < Minitest::Test
   def test_it_exists
     assert_instance_of Shift, @shift
   end
+
+  def test_find_all_keys
+    assert_equal [2, 27, 71, 15], @shift.keys("02715")
+  end
+
+
 end
